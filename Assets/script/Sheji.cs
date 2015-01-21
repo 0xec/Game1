d6f12ee3feb73971a72ff0282cd57b4d0b272875
xec <xec@live.com>
2015-01-21 17:35:23 +0800

    增加脚本

--- Assets/script/Sheji.cs
+++ Assets/script/Sheji.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Sheji : MonoBehaviour {
 
+	int speed = 5;
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		int x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		int z = Input.GetAxis ("Vertical") * Time.deltaTime * speed;
+		transform.Translate (x, 0, z);
+		print (x);
 	}
 }
